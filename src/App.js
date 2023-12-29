461e091693e469b52e08982c107f51b7be5e38e9
Toni Denev <tonidenevbtc@gmail.com>
2023-12-29 16:11:20 +0200

    Add initial routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Martial Art Events</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Martial Art Events</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Events</a>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/events">Events</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">My Profile</a>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/profile">My Profile</Link>
                         </li>
                     </ul>
                 </div>
