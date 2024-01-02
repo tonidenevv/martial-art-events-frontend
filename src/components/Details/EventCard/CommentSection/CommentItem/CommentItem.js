24394e8ce9812cbe3e873a6e233ed279be956b34
Toni Denev <tonidenevbtc@gmail.com>
2024-01-02 15:19:36 +0200

    finish up comment functionality

--- src/components/Details/EventCard/CommentSection/CommentItem/CommentItem.js
+++ src/components/Details/EventCard/CommentSection/CommentItem/CommentItem.js
@@ -1,12 +1,9 @@
-const CommentItem = () => {
+const CommentItem = ({ comment }) => {
     return (
-        <div className="card mt-4">
-            <div className="card-header">Comments</div>
-            <div className="card-body">
-                <blockquote className="blockquote mb-0">
-                    <p>Nice event...</p>
-                </blockquote>
-            </div>
+        <div className="card-body">
+            <blockquote className="blockquote mb-0">
+                <p>{comment}</p>
+            </blockquote>
         </div>
     )
 }
--- src/components/Details/EventCard/CommentSection/CommentSection.js
+++ src/components/Details/EventCard/CommentSection/CommentSection.js
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CommentItem from "./CommentItem/CommentItem";
 import * as eventService from '../../../../services/eventService';
+import { AuthContext } from "../../../../contexts/AuthContext";
+import { useContext } from "react";
 
 const CommentSection = ({ eventId, token, eventOwnerId }) => {
     const [comment, setComment] = useState('');
     const [comments, setComments] = useState([]);
+    const [isOwner, setIsOwner] = useState(false);
+    const [isUser, setIsUser] = useState(false);
     const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { auth } = useContext(AuthContext);
+
+    useEffect(() => {
+        setIsLoading(true);
+        setIsUser(!!auth?.token);
+        eventService.getOne(eventId)
+            .then(res => {
+                setIsLoading(false);
+                setIsOwner(auth?._id === res._ownerId);
+                setComments(res.comments);
+            });
+    }, [eventId, auth?._id, auth?.token]);
 
     const handleSubmit = () => {
         if (comment.length < 5 || comment.length > 20) {
             setError('Comment should be between 5 and 20 characters');
         } else {
-            console.log(comment);
+            setIsLoading(true);
             eventService.comment(comment, eventId, token, eventOwnerId)
-                .then(res => console.log(res))
+                .then(res => {
+                    setIsLoading(false);
+                    setComments(old => [...old, res])
+                })
                 .catch(err => console.log(err));
             setError('');
             setComment('');
@@ -21,23 +42,37 @@ const CommentSection = ({ eventId, token, eventOwnerId }) => {
     }
 
     return (
-        <div className="row">
-            {error && <div style={{ color: 'red' }}>{error}</div>}
-            <div className="col">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Nice event..."
-                    aria-label="comment"
-                    value={comment}
-                    onChange={(e) => setComment(e.target.value)}
-                />
+        isLoading ?
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border text-primary" style={{ width: '6rem', height: '6rem' }} role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
             </div>
-            <div className="col">
-                <button onClick={handleSubmit} type="button" className="btn btn-primary">Comment</button>
+            :
+            <div className="row">
+                {error && <div style={{ color: 'red' }}>{error}</div>}
+                {(isUser && !isOwner) &&
+                    <>
+                        <div className="col">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Nice event..."
+                                aria-label="comment"
+                                value={comment}
+                                onChange={(e) => setComment(e.target.value)}
+                            />
+                        </div>
+                        <div className="col">
+                            <button onClick={handleSubmit} type="button" className="btn btn-primary">Comment</button>
+                        </div>
+                    </>
+                }
+                <div className="card mt-4">
+                    <div className="card-header">Comments</div>
+                    {comments.length > 0 ? comments.map((x, i) => <CommentItem key={i} comment={x} />) : <div>No comments yet...</div>}
+                </div>
             </div>
-            <CommentItem />
-        </div>
 
     )
 }
